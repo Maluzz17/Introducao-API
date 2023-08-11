f6dad23aca1fec6a6e8d61ab108d2ea1bc54629c
Maluzz17 <132698377+Maluzz17@users.noreply.github.com>
2023-08-11 13:32:47 +0000

    Testando

--- bd.js
+++ bd.js
@@ -15,15 +15,3 @@ async function selectUsuarios() {
 }
 
 export { selectUsuarios };
-
-//bd.js
-async function selectUsuario(id) {
-  const client = await connect();
-  const query = "SELECT * FROM usuario WHERE id = $1";
-  const usuario = [id];
-  const res = await client.query(query, usuario);
-  return res.rows;
-}
-
-//bd.js
-export { selectUsuarios, selectUsuario };
--- index.js
+++ index.js
@@ -15,32 +15,34 @@ app.listen(port, () => {            // Um socket para "escutar" as requisições
 
 //index.js
 import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
 
 dotenv.config();
 
-import { selectUsuarios } from "./bd.js";
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Arthur Porto", // Substitua pelo seu nome
+  });
+});
 
 app.get("/usuarios", async (req, res) => {
-  console.log("Rota GET/usuarios solicitada");
   try {
     const usuarios = await selectUsuarios();
     res.json(usuarios);
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
+
+  console.log("Rota GET/usuarios solicitada");
 });
 
-//index.js
-import { selectUsuarios, selectUsuario } from "./bd.js";
-
-//index.js
-app.get("/usuario/:id", async (req, res) => {
-  console.log("Rota GET /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) res.json(usuario);
-    else res.status(404).json({ message: "Usuário não encontrado!" });
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
