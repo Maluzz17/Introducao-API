2c22fc504a4de93a2885774628a12b7d945cd11d
Maluzz17 <132698377+Maluzz17@users.noreply.github.com>
2023-09-01 17:25:07 +0000

    teste2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
+import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
 import dotenv from "dotenv";
+import express from "express";
+
 dotenv.config();
 
-import express from "express";
-import roteadorUsuario from "./routes/usuario.js";
-import loginRouter from "./routes/login.js";
-
 const app = express();              
-const port = 3000;         
+const port = 3000;                 
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(roteadorUsuario);
-app.use(loginRouter);
+app.use(roteadorLogin);
 
 app.get("/", (req, res) => {        
-    res.json({
-        message: "API para CRUD usuario: https://github.com/Maluzz17/Introducao-API" ,
-    });
-    console.log("Rota / solicitada");
+  res.json({
+    nome: "Davi Gomes Mendes",     
+  });
+  console.log("Rota / solicitada");
 });
 
-app.listen(port, () => {
-    console.log(`Serviço escutando na porta:  ${port}`);
+app.listen(port, () => {           
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
